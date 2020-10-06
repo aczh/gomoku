362ed5bcd00dfce1d5b9e3a5d3805408611e0758
Allen Zhang <allenczhang@gmail.com>
2020-10-06 12:32:12 -0500

    removed unneeded code

--- gomoku/threat/threat_search.py
+++ gomoku/threat/threat_search.py
@@ -36,72 +36,67 @@ def get_threats(board, current=True):
     return threats
 
 def _get_threes(board, current=True):
+    # - - - o o - -
+    # - - o - o - -
+    # - - o o - - -
+    straight_three_lookup = [
+        [ [2], [0, 1, 2, 5, 6], [1, 5], [3, 4] ],
+        [ [3], [0, 1, 3, 5, 6], [1, 5], [2, 4] ],
+        [ [4], [0, 1, 4, 5, 6], [1, 5], [2, 3] ],
+    ]
+
     # 12: - o o ! ? -
     # 13: - o ! o ? -
     # 14: - o ? ? o -
     # 23: - ! o o ! -
     # 24: - ? o ! o -
     # 34: - ? ! o o -
-    lookup = {
-        (1, 2): [[ [3, 4] ], [ [4, 3] ]],
-        (1, 3): [[ [2, 4] ], [ [4, 2] ]],
-        (1, 4): [[], [ [2, 3], [3, 2] ]],
-        (2, 3): [[ [1, 4], [4, 1] ], []],
-        (2, 4): [[ [3, 1] ], [ [1, 3] ]],
-        (3, 4): [[ [2, 1] ], [ [1, 2] ]],
-    }
+    three_lookup = [
+        [ [3, 4], [0, 3, 4, 5], [1, 2], [4] ],
+        [ [2, 4], [0, 2, 4, 5], [1, 3], [4] ],
+        [ [2, 3], [0, 2, 3, 5], [1, 4], [2, 3] ],
+        [ [1, 4], [0, 1, 4, 5], [2, 3], [] ],
+        [ [1, 3], [0, 1, 3, 5], [2, 4], [1] ],
+        [ [1, 2], [0, 1, 2, 5], [3, 4], [1] ],
+    ]
 
     b = board.get_board(current=current)
     ret = []
     for inc in [1, board.size, board.size + 1, board.size - 1]:
         cur = {}
-        # =============
-        # - - o o o - -
-        # =============
-        # - - - o o - -
-        # - - o - o - -
-        # - - o o - - -
-        for x in range(3):
-            r = [z + 2 for z in range(3) if z != x]
-            emp = [z for z in range(7) if z not in r]
+        for gain, emp, cost, rest in straight_three_lookup:
             e_bits = (board.e >> inc * emp[0]) & (board.e >> inc * emp[1]) & (board.e >> inc * emp[2]) & (board.e >> inc * emp[3]) & (board.e >> inc * emp[4])
-
-            bits = (b >> inc * r[0]) & (b >> inc * r[1])
+            bits = (b >> inc * rest[0]) & (b >> inc * rest[1])
             bits = bits & threat_mask[7][inc] & e_bits
 
             for o in get_ones(bits):
-                gain = o + (x + 2) * inc
-                rest = [o + (i + 2) * inc for i in range(3) if i != x]
-                cost = [o + inc, o + inc * 5]
-                cur[gain] = threat.Three(gain=gain, cost=cost, rest=rest, inc=inc)
+                gain_square = o + gain[0] * inc
+                cur[gain_square] = threat.Three(
+                    gain=gain_square,
+                    cost=list([o + c * inc for c in cost]),
+                    rest=list([o * c * inc for c in rest]),
+                    inc=inc
+                )
 
-        for x in range(1, 4):
-            for y in range(x + 1, 5):
-                emp = [z for z in range(6) if z != x and z != y]
-                e_bits = (board.e >> inc * emp[0]) & (board.e >> inc * emp[1]) & (board.e >> inc * emp[2]) & (board.e >> inc * emp[3])
+        for gains, emp, rest, broken in three_lookup:
+            e_bits = (board.e >> inc * emp[0]) & (board.e >> inc * emp[1]) & (board.e >> inc * emp[2]) & (board.e >> inc * emp[3])
+            bits = (b >> inc * rest[0]) & (b >> inc * rest[1])
+            bits = bits & threat_mask[6][inc] & e_bits
 
-                bits = (b >> inc * x) & (b >> inc * y)
-                bits = bits & threat_mask[6][inc] & e_bits
+            for o in get_ones(bits):
+                for i, gain in enumerate(gains):
+                    gain_square = o + gain * inc
+                    cost_square = o + gains[i - 1] * inc
+                    if gain_square not in cur:
+                        threat_type = threat.ThreatType.BROKEN_THREE if gain in broken else threat.ThreatType.THREE
 
-                for o in get_ones(bits):
-                    for gain, cost in lookup[(x, y)][0]:
-                        gain_index = o + gain * inc
-                        if gain_index not in cur:
-                            cur[gain_index] = threat.Three(
-                                gain=gain_index,
-                                cost=[o, o + inc * 5, o + inc * cost],
-                                rest=[o + x * inc, o + y * inc],
-                                inc=inc
-                            )
-                    for gain, cost in lookup[(x, y)][1]:
-                        gain_index = o + gain * inc
-                        if gain_index not in cur:
-                            cur[gain_index] = threat.BrokenThree(
-                                gain=gain_index,
-                                cost=[o, o + inc * 5, o + inc * cost],
-                                rest=[o + x * inc, o + y * inc],
-                                inc=inc
-                                )
+                        cur[gain_square] = threat.Threat(
+                            gain=gain_square,
+                            cost=[o, o + inc * 5, cost_square],
+                            rest=[o + r * inc for r in rest],
+                            inc=inc,
+                            type=threat_type,
+                        )
         ret += cur.values()
     return ret
 
