a9a393058024c13957391b3b1029dec5c860228c
Allen Zhang <allenczhang@gmail.com>
2020-10-07 10:57:35 -0500

    displaying something

--- server/server.py
+++ server/server.py
@@ -1,16 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import mongoengine as me
 
 from api.user import api as user
 from api.game import api as game
 
 # initialize flask
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static/dist', template_folder='../static/dist', static_url_path="")
 app.register_blueprint(user)
 app.register_blueprint(game)
 
 # connect mongoengine
 me.connect('gomoku')
+
+@app.route('/', methods=["GET"])
+def serve_bundle():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
