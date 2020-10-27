fe97b067a7b053a94d3c6f3035f81d43f6714a15
Allen Zhang <allenczhang@gmail.com>
2020-10-27 00:43:32 -0500

    fixed bug

--- static/src/components/game/Game.js
+++ static/src/components/game/Game.js
@@ -17,7 +17,9 @@ const Game = ({updateGame, setDialogVisibility}) => {
             updateGame(data.p1, data.p2, data.turns, data.history)
         })
         socket.on('game_won', (data) => {
-            updateGame(data.p1, data.p2, data.turns, data.history)
+            // set turns to turns + 1 so it isn't our move
+            // this prevents users from making additional moves (hacky, but works)
+            updateGame(data.p1, data.p2, data.turns + 1, data.history)
             setDialogVisibility(true)
         })
 
